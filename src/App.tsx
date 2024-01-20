9b63e54fb48d93a536ed0b9923fb3e70a0a88da8
natsukikokubu <natsukikokubu3@gmail.com>
2024-01-21 00:23:21 +0900

    tailwind修正

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
+      <h1 className="text-5xl font-bold underline text-blue-200">
+        Hello World!
+      </h1>
       <div></div>
     </>
   );
